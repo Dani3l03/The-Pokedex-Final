06e825a9a03c795284a78977d73bd6f89ea1d7f2
DanielMacmin <danimacielcomin@hotmail.com>
2024-03-22 12:56:19 -0300

    22/03.2

--- js/fetchAll.js
+++ js/fetchAll.js
@@ -42,46 +42,130 @@ export default function fetchAllPokemon(){
       card.classList.add("pokemon-container-result");
 
       // MODAL - criação e fetch.
+      async function createModalData(data) {
+        card.addEventListener('click', async () => {
+            console.log(`Card de ${data.name} clicado!`)     
+    
+            const pokemonAbilities = data.abilities[0].ability.name;
+            const pokemonHeight = `${(data.height)/10} m`
+            const pokemonWeight = `${(data.weight)/10} kg`;
+            const pokemonHp = data.stats[0].base_stat;
+            const pokemonAttack = data.stats[1].base_stat;
+            const pokemonDefense = data.stats[2].base_stat;
+            const pokemonSpAttack = data.stats[3].base_stat;
+            const pokemonSpDefense = data.stats[4].base_stat;
+            const pokemonSpeed = data.stats[5].base_stat;
 
-     async function createModalData(data){
-      card.addEventListener('click', () => {
-        console.log(`Card de ${data.name} clicado!`)
+            console.log(pokemonAbilities);
+            console.log(pokemonHeight);
+            console.log(pokemonWeight);
+            console.log(pokemonHp);
+            console.log(pokemonAttack);
+            console.log(pokemonDefense);
+            console.log(pokemonSpAttack);
+            console.log(pokemonSpDefense);
+            console.log(pokemonSpeed); 
+    
+            const pokemonTypeSearched = data.types.map((type) => type.type.name);
+    
+            console.log("Tipos do Pokémon:", pokemonTypeSearched);
+    
+           
+            for (const type of pokemonTypeSearched) {
+                const pokeApiType = `https://pokeapi.co/api/v2/type/${type}`;
+                    const result = await fetch(pokeApiType);
+                    const dataType = await result.json();
+                    
+                    console.log(`Dados do tipo ${type}:`, dataType);
+                    const pokemonWeaknesses = dataType.damage_relations.double_damage_from;
+                    console.log(pokemonWeaknesses);
 
-        const pokemonAbilities = data.abilities[0].ability.name;
-        const pokemonHeight = `${(data.height)/10} m`
-        const pokemonWeight = `${(data.weight)/10} kg`;
-        const pokemonHp = data.stats[0].base_stat;
-        const pokemonAttack = data.stats[1].base_stat;
-        const pokemonDefense = data.stats[2].base_stat;
-        const pokemonSpAttack = data.stats[3].base_stat;
-        const pokemonSpDefense = data.stats[4].base_stat;
-        const pokemonSpeed = data.stats[5].base_stat;
-
-        console.log(pokemonAbilities);
-        console.log(pokemonHeight);
-        console.log(pokemonWeight);
-        console.log(pokemonHp);
-        console.log(pokemonAttack);
-        console.log(pokemonDefense);
-        console.log(pokemonSpAttack);
-        console.log(pokemonSpDefense);
-        console.log(pokemonSpeed);
-        
-        const pokemonTypesClick = document.querySelectorAll(".types");
-
-  pokemonTypesClick.forEach((pokeType) => {
-    pokeType.addEventListener("click", async () => {
-      const type = pokeType.id;
-      const pokeApiType = `https://pokeapi.co/api/v2/type/${type}`;
-      const result = await fetch(pokeApiType);
-      const dataType = await result.json();
-  });
-  });
-
-      });
-     
+                    function createCardModal(){
+                        const modalFade = document.createElement("div");
+                        modalFade.id = "modal-fade";
+                    
+                        const containerModalShow = document.createElement("article");
+                        containerModalShow.classList.add("container-modal-show");
+                    
+                        const buttonClose = document.createElement("div");
+                        buttonClose.classList.add("button-close");
+                        const closeButton = document.createElement("button");
+                        const closeIcon = document.createElement("img");
+                        closeIcon.src = "/img/assets/close.svg";
+                        closeButton.appendChild(closeIcon);
+                        buttonClose.appendChild(closeButton);
+                    
+                        const modalContainer = document.createElement("div");
+                        modalContainer.classList.add("modal-container");
+                    
+                        const modal = document.createElement("div");
+                        modal.classList.add("modal");
+                    
+                        const modalImg = document.createElement("div");
+                        modalImg.classList.add("modal-img");
+                    
+                        const modalType = document.createElement("div");
+                        modalType.classList.add("modal-type");
+                        const typeImage = document.createElement("img");
+                        modalType.appendChild(typeImage);
+                    
+                        const modalImgBack = document.createElement("div");
+                        modalImgBack.classList.add("modal-img-back");
+                        const backImage = document.createElement("img");
+                        backImage.src = pokemonImageSearched;
+                        modalImgBack.appendChild(backImage);
+                    
+                        modalImg.appendChild(modalType);
+                        modalImg.appendChild(modalImgBack);
+                    
+                        const modalInfo = document.createElement("div");
+                        modalInfo.classList.add("modal-info");
+                    
+                        const modalName = document.createElement("div");
+                        modalName.classList.add("modal-name");
+                        const nameHeader = document.createElement("h3");
+                        nameHeader.innerText = pokemonNameSearched;
+                        const numberParagraph = document.createElement("p");
+                        numberParagraph.innerText = pokemonNumberSearched;
+                        modalName.appendChild(nameHeader);
+                        modalName.appendChild(numberParagraph);
+                    
+                        const modalTypes = document.createElement("div");
+                        modalTypes.classList.add("modal-types");
+                        const typeOne = document.createElement("p");
+                        typeOne.id = "type-one";
+                        typeOne.innerText = pokemonTypeSearched[0];
+                        const typeTwo = document.createElement("p");
+                        typeTwo.id = "type-two";
+                        typeTwo.innerText = pokemonTypeSearched[0];
+                        modalTypes.appendChild(typeOne);
+                        modalTypes.appendChild(typeTwo);
+                    
+                        const modalCaracter = document.createElement("div");
+                        modalCaracter.classList.add("modal-caracter");                   
+                    
+                        modalInfo.appendChild(modalName);
+                        modalInfo.appendChild(modalTypes);
+                        modalInfo.appendChild(modalCaracter);
+                        
+                        modal.appendChild(modalImg);
+                        modal.appendChild(modalInfo);
+                    
+                        modalContainer.appendChild(modal);
+                    
+                        containerModalShow.appendChild(buttonClose);
+                        containerModalShow.appendChild(modalContainer);
+                    
+                        modalFade.appendChild(containerModalShow);
+                    
+                        return modalFade;                   
+                    }
+                    
+                    createCardModal();
+            }
+            
+        });
     }
-      
       createModalData(data);
 
       const name = document.createElement("p");
